bf1d6491e77ba5d7efa4b1a787d5edcc46f594ad
vsairesse <vsairesse@gmail.com>
2026-04-28 22:01:09 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,79 @@
+let lim = 12;
+
+function getCourses() {
+  let cs = "";
+
+  for (let i = 0; i < lim; i++) {
+    const c = courses[i];
+    if (c.show) {
+      const _color = `rgb(${random(numColor)}, ${random(numColor)}, ${random(numColor)})`;
+      const color = c.colors ? c.colors[0] : _color;
+      cs += `
+     <div class="course">
+        <div class="top" style="background: ${color}">
+          <!--<strong><i class='la la-graduation-cap'></i> Curso</strong>-->
+          <h3>${c.label}</h3>
+          <a href="./pages/plans/${c.plan || ""}">Plano do curso</i></a>
+        </div>
+        <div class="bottom">
+          <p>EM APENAS ${c.duration}</p>
+          <h2>${formatCurrency(c.price)} Mt</h2>
+          <a href="./signup.html?c=${c.id}">Cadastrar-se</a>
+        </div>
+      </div>
+  `;
+    }
+  }
+
+  document.querySelector(".courses").innerHTML = cs;
+}
+
+getCourses();
+
+document.querySelector("input").addEventListener("input", (e) => {
+  if (!e.target.value) {
+    lim = 12;
+    getCourses()
+    document.querySelector('.btn-more').style.display = 'block';
+    return;
+  }
+
+  document.querySelector('.btn-more').style.display = 'none';
+  let cs = "";
+  for (const c of courses) {
+    const _color = `rgb(${random(numColor)}, ${random(numColor)}, ${random(numColor)})`;
+    const color = c.colors ? c.colors[0] : _color;
+    if (
+      c.label.toLowerCase().includes(e.target.value.toLowerCase()) &&
+      c.show
+    ) {
+      cs += `
+      <div class="course">
+        <div class="top" style="background: ${color}">
+          <!--<strong><i class='la la-graduation-cap'></i> Curso</strong>-->
+          <h3>${c.label}</h3>
+          <a href="./pages/plans/${c.plan || ""}">Plano do curso</i></a>
+        </div>
+        <div class="bottom">
+          <p>EM APENAS ${c.duration}</p>
+          <h2>${formatCurrency(c.price)} Mt</h2>
+          <a href="./signup.html?c=${c.id}">Cadastrar-se</a>
+        </div>
+      </div>
+    `;
+    }
+  }
+
+  document.querySelector(".courses").innerHTML = cs;
+});
+
+document.querySelector('.btn-more').addEventListener('click', e => {
+  lim += 12;
+
+  if (lim > courses.length) {
+    lim = courses.length;
+    e.target.style.display = 'none';
+  }
+
+  getCourses();
+})
---
+++ js/main.js
@@ -0,0 +1,102 @@
+const api_url = window.location.protocol.toLowerCase().includes("s")
+  ? "https://api-master.onrender.com"
+  : "http://localhost:8000";
+
+let menuHidden = true;
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  $(".menu").animate({ left: menuHidden ? 0 : "-80%" }, 500);
+  menuHidden = !menuHidden;
+});
+
+awakeAPI();
+
+setInterval(async () => {
+  awakeAPI();
+}, 30000);
+
+const numColor = 255;
+
+async function awakeAPI() {
+  try {
+    const res = await fetch(api_url);
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const baseUrl = window.location.origin
+console.log( window.location)
+
+const components = {
+  header: `
+  <a href="${baseUrl}" class="simple-logo">
+      <img src="${baseUrl}/assets/img/logo.svg" width="40" alt="logo" />
+    </a>
+
+    <a href="${baseUrl}" class="logo">
+      <img src="${baseUrl}/assets/img/logo.svg" width="40" alt="logo" />
+      <span>Knowledge Center</span>
+    </a>
+    <a href="./signup.html" class="btn-credit"><i class="la la-plus"></i>Cadastrar-se</a>
+    <nav>
+      <a href="">Home</a>
+      <a href="">Artigos</a>
+      <a href="">Podcast</a>
+      <a href="./about.html">Sobre Nós</a>
+      <a href="" class="login">
+        <img src="./assets/img/user.svg" width="17" alt="" />
+      </a>
+    </nav>
+    <button class="hamburger hamburger--spin btn-menu" type="button" aria-label="Menu" aria-controls="navigation">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  `,
+  footer:
+    `
+    <div class="f-left">
+      <img src="${baseUrl}/assets/img/logo.svg" width="60" height="60" alt="logo" />
+    </div>
+    <div class="f-right">
+      <div>
+        <h2>Sobre nós</h2>
+        <ul>
+          <li><a href="./about.html">Missão, Visão e valores</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2>Parceiros</h2>
+        <ul>
+          <li><a href="">Crediinvest</a></li>
+          <li><a href="">PowerController</a></li>
+          <li><a href="">Hermaak</a></li>
+        </ul>
+      </div>
+      <div>
+        <h2>Contacto</h2>
+        <ul>
+          <li><a href="">(+258) 872081978</a></li>
+          <li><a href="mailto:knowledgecentermz@gmail.com">knowledgecentermz@gmail.com</a></li>
+          <li><a href="">Tech Space nº 404</a></li>
+        </ul>
+
+        <div class="rs">
+          <a href=""><i class="la la-lg la-facebook"></i></a>
+          <a href=""><i class="la la-lg la-instagram"></i></a>
+          <a href=""><i class="la la-lg la-youtube"></i></a>
+        </div>
+      </div>
+    </div>
+    `
+
+}
+
+
+// COMPNNT
+document.querySelector('header').innerHTML = components.header;
+document.querySelector('footer').innerHTML = components.footer;
